b29460e55d68015f5e5cd6d9c58c71b5098b39b3
sonicisreal <mdr79855@uga.edu>
2024-11-10 22:41:08 -0500

    Added DailyView and WeeklySummary components
    It still needs the daily card. I saw someone else was working on it, so i didn't want to add my own version here.

---
+++ my-app/src/app/components/WeeklySummary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function WeeklySummary() {
+    return (
+        <div>
+            <h2>Weekly Summary</h2>
+            <div>Favorite Muscle Group(s)</div>
+            <div>Total Reps</div>
+            <div>Total Weight Lifted</div>
+        </div>
+    );
+};
