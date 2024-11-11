0ecf80bcb68d3635c1d47aa90ad4fa843f84ee53
ehj72120 <ehj72120@uga.edu>
2024-11-11 15:10:24 -0500

    Implemented Nav bar on page.tsx and login/page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import React from 'react';
 import DailyView from './components/DailyView';
+import {usePathname} from 'next/navigation';
+import Nav from './components/Nav';
 
 
 const Page: React.FC = () => {
+  const pathname = usePathname();
+  console.log("current path:" + pathname);
+
   return (
     <div>
+      <Nav changeView="/login" />
       <DailyView />
     </div>
   );
