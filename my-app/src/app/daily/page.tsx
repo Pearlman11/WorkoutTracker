e5a9974e9dd78a5ec5a0955512f145cff69beaa6
sonicisreal <mdr79855@uga.edu>
2024-11-11 20:20:55 -0500

    Update page.tsx

--- my-app/src/app/daily/page.tsx
+++ my-app/src/app/daily/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import DailyView from "../components/DailyView";
+import Nav from "../components/Nav";
 
 export default function DailyPage() {
     return (
         <div>
-            <DailyView />
+            <div className='nav'>
+                <Nav changeView="/login" />
+            </div>
+            <div className='dv'>
+                <DailyView />
+            </div>
         </div>
     );
 }
